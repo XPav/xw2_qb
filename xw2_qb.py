37b191c8067566d11c37cf175ecce868091afd7c
Alex Pavloff <alex@augmntr.com>
2019-06-16 11:59:10 -0700

    working

--- xw2_qb.py
+++ xw2_qb.py
@@ -1,6 +1,28 @@
 import json
 import os
 
+def PrintPilot(pilot):
+    fullpilot = PILOTS[pilot['id']]
+    ship = SHIPS[fullpilot['ship']]
+
+    s = f"{ship['name']} - {fullpilot['name']}"
+
+    if 'upgrades' in pilot:
+        for upgrade in pilot['upgrades'].values():
+            for card in upgrade:
+                fullcard = UPGRADES[card]
+                s += f"+{fullcard['name']}"
+    return s
+
+def PrintPilots( pilots ):
+    if len(pilots) > 2:
+        raise ValueError
+    if len(pilots) > 1:
+        return f'{PrintPilot(pilots[0])},{PrintPilot(pilots[1])}'
+    else:
+        return f'{PrintPilot(pilots[0])},'
+
+
 path = 'c:\\dev\\xwing-data2\\data\\'
 
 # Read ships and pilots
@@ -28,8 +50,7 @@ for r, d, f in os.walk(path + 'upgrades'):
                 upgrade_json = json.load(json_file)
                 for upgrade in upgrade_json:
                     if 'cost' in upgrade:
-                        UPGRADES[upgrade['xws']] = upgrade['cost']
-
+                        UPGRADES[upgrade['xws']] = upgrade
 
 # Read quick builds                        
 QBS = []                        
@@ -40,6 +61,7 @@ for r, d, f in os.walk(path + 'quick-builds'):
             with open(filename) as json_file:
                 qb_json = json.load(json_file)
                 for qb in qb_json['quick-builds']:
+                    qb['faction'] = os.path.splitext(file)[0]
                     QBS.append( qb )
 
 # Add cost to quickbuilds
@@ -51,7 +73,7 @@ for qb in QBS:
         if 'upgrades' in pilot:
             for upgrade in pilot['upgrades'].values():
                 for card in upgrade:
-                    upgradecost = UPGRADES[card]
+                    upgradecost = UPGRADES[card]['cost']
                     if 'variable' in upgradecost:
                         if upgradecost['variable'] == 'agility':
                             for stat in ship['stats']:
@@ -70,8 +92,14 @@ for qb in QBS:
                     else:
                         cost += upgradecost['value']
     qb['cost'] = cost
+    qb['ratio'] = float(cost) / qb['threat']
 
-# Print out to CSV
-print(QBS)
+# Print out to CSV.  Faction, Ship0, Ship1, TL, Cost, Cost/TL
+with open('xw2_qb.csv', 'w') as csv:
+    csv.write('Faction, Ship0, Ship1, Threat, Cost, Ratio\n')
+    for qb in QBS:
+        s = f"{qb['faction']},{PrintPilots(qb['pilots'])},{qb['threat']},{qb['cost']},{qb['ratio']},\n"
+        csv.write( s )
+
 
 
